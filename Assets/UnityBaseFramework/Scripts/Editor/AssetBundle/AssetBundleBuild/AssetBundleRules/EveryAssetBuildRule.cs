ff2607503c5a673a659b88eade2d7651f6800180
cofdream <cofdream@sina.com>
2021-11-12 17:48:52 +0800

    新增打包规则

---
+++ Assets/UnityBaseFramework/Scripts/Editor/AssetBundle/AssetBundleBuild/AssetBundleRules/EveryAssetBuildRule.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Cofdream.AssetEditor
+{
+    /// <summary>
+    /// 每个资源文件都打单独AB包
+    /// </summary>
+    public class EveryAssetBuildRule : ScriptableObject, IBuildRule
+    {
+        public Object AssetFolder;
+
+        public void CreateAssetBundleBuild(CreateCallback createCallback)
+        {
+            string path = AssetDatabase.GetAssetPath(AssetFolder);
+
+            var folders = AssetDatabase.GetSubFolders(path);
+
+            for (int i = 0; i < folders.Length; i++)
+            {
+                createCallback(new AssetBundleBuild()
+                {
+                    assetBundleName = BuildRuleUtil.PathToAssetBundleName(folders[i]),
+                    assetNames = new string[] { folders[i] },
+                });
+            }
+        }
+        private void OnValidate()
+        {
+            if (AssetFolder != null)
+            {
+                string path = AssetDatabase.GetAssetPath(AssetFolder);
+                if (AssetDatabase.IsValidFolder(path) == false)
+                {
+                    EditorUtility.DisplayDialog("警告", "不是文件夹路径", "确认");
+                }
+            }
+        }
+    }
+}
