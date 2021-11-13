0cfa509f2942917a35cc7a0e8f9b6b50bfddceb4
cofdream <cofdream@sina.com>
2021-11-13 14:20:38 +0800

    修改打包规则

--- Scripts/Editor/AssetBundle/AssetBundleBuild/AssetBundleRules/CustomBuildRule.cs
+++ Scripts/Editor/AssetBundle/AssetBundleBuild/AssetBundleRules/CustomBuildRule.cs
@@ -22,4 +22,35 @@ namespace Cofdream.AssetEditor
             });
         }
     }
+
+    [CustomEditor(typeof(CustomBuildRule))]
+    [CanEditMultipleObjects]
+    public class CustomBuildRuleInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("包名 / \\ 转换成 _"))
+            {
+                foreach (var item in targets)
+                {
+                    var obj = (CustomBuildRule)item;
+                    obj.AssetBundleName = obj.AssetBundleName.Replace('/', '_').Replace('\\', '_');
+                }
+            }
+            if (GUILayout.Button("资源名数组 / \\ 转换成 _"))
+            {
+                foreach (var item in targets)
+                {
+                    var obj = (CustomBuildRule)item;
+                    int length = obj.AssetNames.Length;
+                    for (int i = 0; i < length; i++)
+                    {
+                        obj.AssetNames[i] = obj.AssetNames[i].Replace('/', '_').Replace('\\', '_');
+                    }
+                }
+            }
+        }
+    }
 }
